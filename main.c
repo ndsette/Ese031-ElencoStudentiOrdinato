c76ef7af9f13647f34c82db213a9e982cda883e3
Diego De Leonardis <diegowazzaap@hotmail.it>
2015-04-15 12:44:48 +0200

    miasoluzione

--- main.c
+++ main.c
@@ -1,26 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
-// Definisci le costanti
-
-// Definisci il tipo di dato struct s_studente
-
-// Definisci studente come struct s_studente
-
-// Dichiara il prototipo della funzione di ordinamento
-
-
+int const N= 3;
+void ordinamento(a[],int N);
 int main(int argc, char** argv) {
-    // Dichiara le variabili locali
     
-    // Codice per l'inserimento, da parte dell'utente, dei dati
+    int i,j;
     
-    // Ordina l'elenco (con funzione)
+    int min;
+
+    struct s_alunno {
     
-    // Codice per la stampa dei dati (uno studente per riga)
+        
+        char cognome [10];
+        char nome [10];
+        int classe,eta;
+        
+    };
+    struct s_alunno cambio; 
+    struct s_alunno a[N];
     
+    for(i=0;i<N;i++){
+        
+        printf("nome: ",a[i].nome);
+        scanf("%s",&a[i].nome);
+        printf("conome: ",a[i].cognome);
+        scanf("%s",&a[i].cognome);
+        printf("classe: ",a[i].classe);
+        scanf("%d",&a[i].classe);
+        printf("eta: ",a[i].eta);
+        scanf("%d",&a[i].eta);
+    }
+    ordinamento(a[],int N);
+    
+    for(i=0;i<N;i++){
+        
+     printf("nome:  %s  cognome:  %s    classe:  %d  eta:   %d",a[i].nome,a[i].cognome,a[i].classe,a[i].eta);
+        
+    }
+  void ordinamento(a[],int N){
+      struct s_alunno scambio;
+    for(i=0;i<N;i++){
+        
+        min=i;
+        
+        for(j=i+1;j<N;j++){
+            
+            if(strcmp(a[j].cognome,a[min].cognome)<0){
+                min=j;
+            }
+        }
+        
+      if(j!=min){
+          scambio=a[min];
+          a[min]=a[j];
+          a[j]=scambio;
+      }  
+    }
+    }
     return (EXIT_SUCCESS);
 }
-
-// Definisci la funzione di ordinamento
+  
